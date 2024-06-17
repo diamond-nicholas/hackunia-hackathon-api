33d4e9aefb276d957f1d16aa8003d8dbbc12bb92
nicholasdiamond <diamondnicholas@talstrike.com>
2024-06-17 13:08:33 +0100

    update the resource model

---
+++ src/models/resource.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const ResourceSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["hospital", "food center", "shelter"],
+    required: true,
+  },
+  location: {
+    type: { type: String, default: "Point" },
+    coordinates: { type: [Number], required: true }, // [longitude, latitude]
+    index: true,
+  },
+  availability: {
+    type: String,
+    enum: ["available", "not available"],
+    default: "available",
+  },
+  createdBy: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+// ResourceSchema.index({ location: "2dsphere" }); // Index for geospatial queries
+
+const Resource = mongoose.model("Resource", ResourceSchema);
+
+module.exports = Resource;
